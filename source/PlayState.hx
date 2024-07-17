36fac094a69d025112de1f7f3f3ca314b1cdd637
Rico Webb <119141397+ricoanimations@users.noreply.github.com>
2024-07-17 14:26:46 -0400

    added placement text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -25,6 +25,22 @@ class PlayState extends FlxState
 	var bar14 = new FlxSprite();
 	var bar15 = new FlxSprite();
 	var bar16 = new FlxSprite();
+	var barPlacementText1 = new FlxText();
+	var barPlacementText2 = new FlxText();
+	var barPlacementText3 = new FlxText();
+	var barPlacementText4 = new FlxText();
+	var barPlacementText5 = new FlxText();
+	var barPlacementText6 = new FlxText();
+	var barPlacementText7 = new FlxText();
+	var barPlacementText8 = new FlxText();
+	var barPlacementText9 = new FlxText();
+	var barPlacementText10 = new FlxText();
+	var barPlacementText11 = new FlxText();
+	var barPlacementText12 = new FlxText();
+	var barPlacementText13 = new FlxText();
+	var barPlacementText14 = new FlxText();
+	var barPlacementText15 = new FlxText();
+	var barPlacementText16 = new FlxText();
 
 	override public function create()
 	{
@@ -80,6 +96,69 @@ class PlayState extends FlxState
 
 		bar16 = new FlxSprite(100, 675).makeGraphic(500, 40, 0xFF006400);
 		add(bar16);
+		barPlacementText1 = new FlxText(25, 0, 0, '1st', 32);
+		barPlacementText1.setFormat("Times New Roman", 32);
+		add(barPlacementText1);
+
+		barPlacementText2 = new FlxText(25, 45, 0, '2nd', 32);
+		barPlacementText2.setFormat("Times New Roman", 32);
+		add(barPlacementText2);
+
+		barPlacementText3 = new FlxText(25, 90, 0, '3rd', 32);
+		barPlacementText3.setFormat("Times New Roman", 32);
+		add(barPlacementText3);
+
+		barPlacementText4 = new FlxText(25, 135, 0, '4th', 32);
+		barPlacementText4.setFormat("Times New Roman", 32);
+		add(barPlacementText4);
+
+		barPlacementText5 = new FlxText(25, 180, 0, '5th', 32);
+		barPlacementText5.setFormat("Times New Roman", 32);
+		add(barPlacementText5);
+
+		barPlacementText6 = new FlxText(25, 225, 0, '6th', 32);
+		barPlacementText6.setFormat("Times New Roman", 32);
+		add(barPlacementText6);
+
+		barPlacementText7 = new FlxText(25, 270, 0, '7th', 32);
+		barPlacementText7.setFormat("Times New Roman", 32);
+		add(barPlacementText7);
+
+		barPlacementText8 = new FlxText(25, 315, 0, '8th', 32);
+		barPlacementText8.setFormat("Times New Roman", 32);
+		add(barPlacementText8);
+
+		barPlacementText9 = new FlxText(25, 360, 0, '9th', 32);
+		barPlacementText9.setFormat("Times New Roman", 32);
+		add(barPlacementText9);
+
+		barPlacementText10 = new FlxText(25, 405, 0, '10th', 32);
+		barPlacementText10.setFormat("Times New Roman", 32);
+		add(barPlacementText10);
+
+		barPlacementText11 = new FlxText(25, 450, 0, '11th', 32);
+		barPlacementText11.setFormat("Times New Roman", 32);
+		add(barPlacementText11);
+
+		barPlacementText12 = new FlxText(25, 495, 0, '12th', 32);
+		barPlacementText12.setFormat("Times New Roman", 32);
+		add(barPlacementText12);
+
+		barPlacementText13 = new FlxText(25, 540, 0, '13th', 32);
+		barPlacementText13.setFormat("Times New Roman", 32);
+		add(barPlacementText13);
+
+		barPlacementText14 = new FlxText(25, 585, 0, '14th', 32);
+		barPlacementText14.setFormat("Times New Roman", 32);
+		add(barPlacementText14);
+
+		barPlacementText15 = new FlxText(25, 630, 0, '15th', 32);
+		barPlacementText15.setFormat("Times New Roman", 32);
+		add(barPlacementText15);
+
+		barPlacementText16 = new FlxText(25, 675, 0, '16th', 32);
+		barPlacementText16.setFormat("Times New Roman", 32);
+		add(barPlacementText16);
 	}
 
 	override public function update(elapsed:Float)
